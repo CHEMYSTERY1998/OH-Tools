cac381c4b63a2eb68a82e8ee4a3173cf02d82151
chemystery <314689545@qq.com>
2025-04-09 17:36:39 +0800

    dev

--- src/extension.ts
+++ src/extension.ts
@@ -1,108 +1,25 @@
 import * as vscode from 'vscode';
-import {platform} from "os";
 
-import { processCallStack } from './utils';
-
-/**
- * 获取当前活动编辑器中光标所在行的文本内容
- * @returns 当前行的文本，如果没有活动编辑器则返回 undefined
- */
-export function getCurrentLineText(): string | undefined {
-    const editor = vscode.window.activeTextEditor; // 获取当前活动的编辑器
-    if (!editor) {
-        return undefined; // 如果没有活动编辑器，返回 undefined
-    }
-
-    const document = editor.document; // 当前文档
-    const selection = editor.selection; // 当前光标或选择范围
-    const currentLine = document.lineAt(selection.active.line); // 获取光标所在行
-    return currentLine.text; // 返回当前行的文本
-}
-
-export function getCurrentLineNum(useRelativePath: boolean = false): string | undefined {
-    const editor = vscode.window.activeTextEditor;
-    if (!editor) {
-        return undefined;
-    }
-    let separator = "\\";
-    if (platform() !== "win32") {
-        separator = "/";
-    }
-    const document = editor.document; // 当前文档
-    const fileName = editor.document.fileName.split(separator).pop();
-    const selection = editor.selection;
-    const currentLine = document.lineAt(selection.active.line);
-    const filePath = useRelativePath ? vscode.workspace.asRelativePath(document.fileName) : fileName;
-    return filePath + ":" + currentLine.lineNumber.toString();
-}
+import { Comander } from './comand';
 
 export function activate(context: vscode.ExtensionContext) {
     console.log('Congratulations, your extension "oh-tools" is now active!');
-
-    const getSingleTest = vscode.commands.registerCommand('oh-tools.getSingleTest', () => {
-        const currentLineText = getCurrentLineText();
-        if (currentLineText === undefined) {
-            vscode.window.showInformationMessage('未找到活动编辑器。');
-            return;
-        }
-        // vscode.window.showInformationMessage(`当前行内容: ${currentLineText}`);
-        const regex = /\((\w+?),\s*(\w+),\s+([\w|\.]+)\)/; // 定义正则表达式
-        const match = currentLineText.match(regex); // 匹配字符串
-        if (!match) {
-            vscode.window.showErrorMessage("未匹配到测试用例.");
-            return;
-        }
-        const [, testClassName, testName, testLevel] = match; // 使用数组解构将三个组存储到变量中
-        const testCmd = `--gtest_filter=${testClassName}.${testName}`;
-        vscode.env.clipboard.writeText(testCmd).then(() => {
-            vscode.window.showInformationMessage('命令已复制到剪贴板！');
-        });
-    });
-
+    // 获取GTest单个用例命令
+    const getSingleTest = vscode.commands.registerCommand('oh-tools.getSingleTest', Comander.getSingleTestCommand);
     // 获取当前活动行号
-    const getLineNum = vscode.commands.registerCommand('oh-tools.getLineNum', () => {
-        const currentLineNum =getCurrentLineNum(false); // 获取当前行号
-        if (currentLineNum === undefined) {
-            vscode.window.showInformationMessage('未找到活动编辑器。');
-            return;
-        }
-        vscode.env.clipboard.writeText(currentLineNum).then(() => {
-            vscode.window.showInformationMessage('文件所在行已复制到剪贴板！');
-        });
-    });
-
-    const getPathLineNum = vscode.commands.registerCommand('oh-tools.getPathLineNum', () => {
-        const currentLineNum =getCurrentLineNum(true); // 获取当前行号
-        if (currentLineNum === undefined) {
-            vscode.window.showInformationMessage('未找到活动编辑器。');
-            return;
-        }
-        vscode.env.clipboard.writeText(currentLineNum).then(() => {
-            vscode.window.showInformationMessage('文件所在行已复制到剪贴板！');
-        });
-    });
-
+    const getLineNum = vscode.commands.registerCommand('oh-tools.getLineNum', Comander.getLineNumCommand);
+    // 获取当前活动行号(带路径)
+    const getPathLineNum = vscode.commands.registerCommand('oh-tools.getPathLineNum', Comander.getPathLineNumCommand);
     // @command:editor.action.sortLinesAscending
-    const sortAtoZ = vscode.commands.registerCommand('oh-tools.sortAtoZ', () => {
-        // 调用内置的降序排序命令
-        vscode.commands.executeCommand('editor.action.sortLinesAscending').then(() => {
-            vscode.window.showInformationMessage('已按行升序序排序！');
-        });
-    });
+    const sortAtoZ = vscode.commands.registerCommand('oh-tools.sortAtoZ', Comander.sortAtoZCommand);
     // @command:editor.action.sortLinesDescending
-    const sortZtoA = vscode.commands.registerCommand('oh-tools.sortZtoA', () => {
-        // 调用内置的降序排序命令
-        vscode.commands.executeCommand('editor.action.sortLinesDescending').then(() => {
-            vscode.window.showInformationMessage('已按行降序排序！');
-        });
-    });
-
-    // getline
-    const getCallLine = vscode.commands.registerCommand('oh-tools.getCallLine', () => {
-        processCallStack();
-    })
-
+    const sortZtoA = vscode.commands.registerCommand('oh-tools.sortZtoA', Comander.sortZtoACommand);
+    // getCallLine
+    const getCallLine = vscode.commands.registerCommand('oh-tools.getCallLine', Comander.getCallLineComand);
+    // setCallInfo
+    const setCallInfo = vscode.commands.registerCommand('oh-tools.setCallInfo', Comander.setCallInfoCommand);
+    // 注册命令
     context.subscriptions.push(getSingleTest, sortAtoZ, sortZtoA, getLineNum, getPathLineNum, getCallLine);
 }
 
-export function deactivate() {}
+export function deactivate() { }
--- src/utils.ts
+++ src/utils.ts
@@ -1,136 +1,100 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
+
+import { platform } from "os";
 import { exec } from 'child_process';
 
-import { OHLOG } from './logger';
+const DEFAULT_BIN_PATH = "gn";
 
-const CODE_BASE_PATH = '/srv/workspace/dev_0324_cc471034f/code';
-const ADDR2LINE_TOOL_PATH = path.join(CODE_BASE_PATH, 'prebuilts/clang/ohos/linux-x86_64/llvm/bin/llvm-addr2line');
-const ABI_TYPE = 'generic_generic_arm_64only';
-const DEVICE_TYPE = 'general_all_phone_standard';
-const CALLSTACK_INFO = `
-#00 pc 00000000001c04fc /system/lib/ld-musl-aarch64.so.1(__timedwait_cp+192)(b1d75fe0aeacc10814a52e8e7263a527)
-#01 pc 00000000001c2650 /system/lib/ld-musl-aarch64.so.1(pthread_cond_timedwait+188)(b1d75fe0aeacc10814a52e8e7263a527)
-#02 pc 00000000000c11f8 /system/lib64/libc++.so(std::__h::condition_variable::__do_timed_wait(std::__h::unique_lock<std::__h::mutex>&, std::__h::chrono::time_point<std::__h::chrono::system_clock, std::__h::chrono::duration<long long, std::__h::ratio<1l, 1000000000l>>>)+108)(636474219bd868a0ae4d237e71f55db2c72e3149)
-#03 pc 00000000001c055c /data/test/HmlV1ProcessorTest(27f1829a3a1274c60db6e270dd305886)
-#04 pc 00000000001bfd60 /data/test/HmlV1ProcessorTest(OHOS::SoftBus::WifiDirectContext::CheckResult(int, OHOS::SoftBus::WifiDirectContext::ResultType)+464)(27f1829a3a1274c60db6e270dd305886)
-#05 pc 00000000001db234 /data/test/HmlV1ProcessorTest(OHOS::SoftBus::HmlV1ProcessorTest_ProcessConnectCommandForCreate01_Test::TestBody()+432)(27f1829a3a1274c60db6e270dd305886)
-#06 pc 000000000020fccc /data/test/HmlV1ProcessorTest(testing::Test::Run()+212)(27f1829a3a1274c60db6e270dd305886)
-#07 pc 0000000000210924 /data/test/HmlV1ProcessorTest(testing::TestInfo::Run()+568)(27f1829a3a1274c60db6e270dd305886)
-#08 pc 000000000021137c /data/test/HmlV1ProcessorTest(testing::TestSuite::Run()+476)(27f1829a3a1274c60db6e270dd305886)
-#09 pc 000000000021e718 /data/test/HmlV1ProcessorTest(testing::internal::UnitTestImpl::RunAllTests()+1224)(27f1829a3a1274c60db6e270dd305886)
-#10 pc 000000000021e0ac /data/test/HmlV1ProcessorTest(testing::UnitTest::Run()+120)(27f1829a3a1274c60db6e270dd305886)
-#11 pc 000000000022e904 /data/test/HmlV1ProcessorTest(main+96)(27f1829a3a1274c60db6e270dd305886)
-#12 pc 00000000000a159c /system/lib/ld-musl-aarch64.so.1(libc_start_main_stage2+80)(b1d75fe0aeacc10814a52e8e7263a527)
-`;
+/**
+ * 获取当前活动编辑器中光标所在行的文本内容
+ * @returns 当前行的文本，如果没有活动编辑器则返回 undefined
+ */
+export function getCurrentLineText(): string | undefined {
+    const editor = vscode.window.activeTextEditor; // 获取当前活动的编辑器
+    if (!editor) {
+        return undefined; // 如果没有活动编辑器，返回 undefined
+    }
 
-function collectZippedSharedLibraries(directory: string): Record<string, string> {
-    const zippedLibraries: Record<string, string> = {};
-    const readDirectoryRecursively = (dir: string): string[] => {
-        const files: string[] = [];
-        const directories: string[] = [];
-        const entries = fs.readdirSync(dir);
-        for (const entry of entries) {
-            const entryPath = path.join(dir, entry);
-            if (fs.statSync(entryPath).isDirectory()) {
-                directories.push(entryPath);
+    const document = editor.document; // 当前文档
+    const selection = editor.selection; // 当前光标或选择范围
+    const currentLine = document.lineAt(selection.active.line); // 获取光标所在行
+    return currentLine.text; // 返回当前行的文本
+}
+
+export function getCurrentLineNum(useRelativePath: boolean = false): string | undefined {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        return undefined;
+    }
+    let separator = "\\";
+    if (platform() !== "win32") {
+        separator = "/";
+    }
+    const document = editor.document; // 当前文档
+    const fileName = editor.document.fileName.split(separator).pop();
+    const selection = editor.selection;
+    const currentLine = document.lineAt(selection.active.line);
+    const filePath = useRelativePath ? vscode.workspace.asRelativePath(document.fileName) : fileName;
+    return filePath + ":" + currentLine.lineNumber.toString();
+}
+
+// 校验可执行文件路径是否有效
+async function validateBinaryPath(BinPath: string): Promise<boolean> {
+    return new Promise((resolve) => {
+        exec(`${BinPath} --version`, (error) => {
+            if (error) {
+                vscode.window.showErrorMessage(`Error: Bin path is incorrect, please input the correct path.`);
+                resolve(false);  // 路径无效
             } else {
-                files.push(entryPath);
+                resolve(true);  // 路径有效
             }
-        }
-        for (const subDir of directories) {
-            files.push(...readDirectoryRecursively(subDir));
-        }
-        return files;
-    };
-
-    const allFiles = readDirectoryRecursively(directory);
-    for (const file of allFiles) {
-        if (file.endsWith('.z.so')) {
-            const fileName = path.basename(file);
-            zippedLibraries[fileName] = file;
-        }
-    }
-    return zippedLibraries;
+        });
+    });
 }
 
-function collectExecutableFiles(directory: string): Record<string, string> {
-    const executableFiles: Record<string, string> = {};
-    const readDirectoryRecursively = (dir: string): string[] => {
-        const files: string[] = [];
-        const directories: string[] = [];
-        const entries = fs.readdirSync(dir);
-        for (const entry of entries) {
-            const entryPath = path.join(dir, entry);
-            if (fs.statSync(entryPath).isDirectory()) {
-                directories.push(entryPath);
-            } else {
-                files.push(entryPath);
-            }
-        }
-        for (const subDir of directories) {
-            files.push(...readDirectoryRecursively(subDir));
-        }
-        return files;
-    };
+/**
+ * 检查路径是否合法
+ * @param inputPath 要验证的路径
+ * @returns 返回路径是否合法以及类型（文件/目录/无效）
+ */
+export function validatePath(inputPath: string): { type: 'file' | 'directory' | 'invalid' } {
+    try {
+        const resolvedPath = path.resolve(inputPath); // 解析成绝对路径
+        const stats = fs.statSync(resolvedPath); // 获取路径信息
 
-    const allFiles = readDirectoryRecursively(directory);
-    for (const file of allFiles) {
-        const fileName = path.basename(file);
-        executableFiles[fileName] = file;
+        if (stats.isFile()) {
+            return { type: 'file' };
+        } else if (stats.isDirectory()) {
+            return { type: 'directory' };
+        } else {
+            return { type: 'invalid' };
+        }
+    } catch (error) {
+        // 如果路径不存在或者出错，则视为无效路径
+        return { type: 'invalid' };
     }
-    return executableFiles;
 }
 
-function getCallStackMap(callStackInfo:string): Record<string, [string, string]> {
-    const callStackMap: Record<string, [string, string]> = {};
-    const regex = /(#\d+)\s+pc\s+(\w+)\s+.*\/(.*\.z\.so|.*Test)\(.*\)/;
-    const lines = callStackInfo.split('\n');
-    for (const line of lines) {
-        if (line.trim().startsWith('#')) {
-            const match = regex.exec(line);
-            if (match) {
-                const frameNumber = match[1];
-                const address = match[2];
-                const libraryName = match[3];
-                callStackMap[address] = [frameNumber, libraryName];
-            }
-        }
-    }
-    return callStackMap;
-}
 
-async function parseCallStack(libraryPaths: Record<string, string>, callStackMap: Record<string,[string, string]>):
-    Promise<void> {
-    let formattedCallStack = '';
-    for (const address of Object.keys(callStackMap)) {
-        const libraryName = callStackMap[address][1];
-        const libraryPath = libraryPaths[libraryName];
-        if (!libraryPath) {
-            continue;
-        }
-        try {
-            // exec(`${ADDR2LINE_TOOL_PATH} -Cfpie ${libraryPath} ${address}`, (error, stdout, stderr) => {
-            //     if (stdout) {
-            //         formattedCallStack += `\x1b[31m${callStackMap[address][0]}\x1b[32m ${stdout}\x1b[0m`
-            //     }
-            // });
-            OHLOG.instance.log(`${ADDR2LINE_TOOL_PATH} -Cfpie ${libraryPath} ${address}`);
-        } catch (error) {
-            console.error(`Error processing address ${address}:`, error);
-        }
+// 弹出输入框让用户输入路径
+async function setPathToConfig(configPath: string, defalutPath: string, fileType: string, prompt: string): Promise<string> {
+    const config = vscode.workspace.getConfiguration();
+    let binPath = await vscode.window.showInputBox({
+        placeHolder: prompt,
+        prompt: prompt,
+        value: defalutPath,
+    });
+    if (!binPath) {
+        return defalutPath;
+    }
+    const ret = validatePath(binPath);
+    // 如果用户输入了合法路径，则保存到配置中
+    if (ret.type === fileType) {
+        config.update(configPath, binPath, vscode.ConfigurationTarget.Workspace);
     }
 
-    // OHLOG.logger.log(formattedCallStack);
-    OHLOG.instance.show();
-}
-
-export async function processCallStack() {
-    const sharedLibraryPath = path.join(CODE_BASE_PATH, 'out', ABI_TYPE, DEVICE_TYPE, 'lib.unstripped');
-    const executablePath = path.join(CODE_BASE_PATH, 'out', ABI_TYPE, DEVICE_TYPE, 'exe.unstripped');
-    const libraryPaths = { ...collectZippedSharedLibraries(sharedLibraryPath), ...collectExecutableFiles(executablePath) };
-    const callStackMap = getCallStackMap(CALLSTACK_INFO);
-    parseCallStack(libraryPaths, callStackMap);
+    return binPath;
 }
 
